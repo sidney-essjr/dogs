b5bb71b510d03a32ede747ef65014663f8421365
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-03-21 09:46:58 -0300

    :tada: Commit inicial

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "dogsapi.origamid.dev" }],
+  },
+};
+
+export default nextConfig;
