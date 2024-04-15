0252c527859a2d91b0f16866d6a8e5e96c7e2d05
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-04-15 09:16:44 -0300

    feat: adiciona o conteudo da page perfil

--- src/app/perfil/[usuario]/page.tsx
+++ src/app/perfil/[usuario]/page.tsx
@@ -1,9 +1,21 @@
+import { obterFotos } from "@/actions/obter-fotos";
+import Feed from "@/app/_componentes/feed/feed";
+
 interface Parametros {
   params: {
     usuario: string;
   };
 }
 
-export default function PerfilUsuarioPage({ params }: Parametros) {
-  return <main>PerfilUsuarioPage</main>;
+export default async function PerfilUsuarioPage({ params }: Parametros) {
+  const { data } = await obterFotos({ user: params.usuario });
+
+  if (!data) return null;
+
+  return (
+    <section className="container mainSection">
+      <h1 className="title">{params.usuario}</h1>
+      <Feed fotos={data} user={params.usuario} />
+    </section>
+  );
 }
