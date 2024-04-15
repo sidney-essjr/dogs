4e5d39085bb6550b69a7ace1415d5e155a10a8d8
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-04-15 09:33:51 -0300

    build app

--- src/functions/verify-token.ts
+++ src/functions/verify-token.ts
@@ -2,7 +2,7 @@ import { jwtVerify } from "jose";
 
 export default async function verifyToken(token: string): Promise<boolean> {
   try {
-    // await jwtVerify(token, new TextEncoder().encode(""), { algorithms: [] });
+    await jwtVerify(token, new TextEncoder().encode(process.env.JWR_SALT), { algorithms: [] });
     return true;
   } catch (error) {
     return false;
